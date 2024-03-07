5580c3248adc01f5fd9bac0228b3986c78e14237
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-03-07 23:25:38 +0600

    thunk api removed

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -1,10 +1,1 @@
-// import { createAsyncThunk } from "@reduxjs/toolkit";
-// import axios from "axios";
 
-// export const fetchEssayGrade = createAsyncThunk(
-//   "essay/fetchGrade",
-//   async (essayData) => {
-//     const response = await axios.post("/api/grade", essayData);
-//     return response.data;
-//   }
-// );
